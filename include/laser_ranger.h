0fd5399738eea76b639d0d8c7bef237a7227b324
Isaac Vandor <isaacvandor@gmail.com>
2019-04-22 10:44:25 -0400

    Pipeline created and working, now need to add camera and laser specifics

---
+++ include/laser_ranger.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+#include "sensor_msgs/LaserScan.h"
+#include "distance_estimator.h"
+
+using namespace cv;
+
+class LaserRanger {
+public:
+    LaserRanger(std::string calibration_file);
+
+    bool Step(const Mat& frame);
+
+    Mat FindFrameMask(const Mat& gray_frame);
+    std::unique_ptr<sensor_msgs::LaserScan> FindLaserCOMs(const Mat& gray);
+
+private:
+    DistanceEstimator distance_estimator_;
+    ros::NodeHandle nh_;
+    ros::Publisher pub_;
+};
